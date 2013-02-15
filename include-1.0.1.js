a32debbea000699f0f2299b06cde13b7f376f413
Ali Najafizadeh <Ali.Najafizadeh@uxpsystems.com>
2013-02-15 18:37:05 -0500

    fixed minot bug and added new feature

--- include-1.0.1.js
+++ include-1.0.1.js
@@ -1,5 +1,5 @@
 /* include.js: Light and Simple dependency manager in JavaScript.
- * version: 0.1.0
+ * version: 0.1.1
  * Author Ali Najafizadeh
  * MIT Licensed.
  */
@@ -8,6 +8,7 @@ var include = (function () {
 
     var loadedModules = {},
         srcPath = {},
+        remaining = 0,
         checkSrcPath = function (name, path) {
             if (srcPath[name] && srcPath[name].path !== path) {
                 throw 'Duplicate source name [' + name + ', ' + srcPath[name] + ']';
@@ -29,7 +30,7 @@ var include = (function () {
          * if target is an object the function callback will have the following signiture
          *     function (name, value)
          */
-        each = function (target, fn) {
+         each = function (target, fn) {
             var i;
             if (target instanceof Array) {
                 if (target && target.length > 0) {
@@ -73,9 +74,26 @@ var include = (function () {
                 }
             }
 
+            function callProgressStatus(filename, name, status) {
+                if('function' === typeof include.status) {
+                    if('loaded' === status) {
+                        remaining--;
+                    }
+                    include.status({
+                        filename: filename,
+                        name: name,
+                        status: status,
+                        remaining: remaining
+                    });
+                }
+            }
+
             return function (name, fn) {
                 var sourceURL = prepareURL(name),
                     script;
+
+                //callProgressStatus(sourceURL, name, 'loading');
+
                 if (sourceURL !== false) { //it means that it has already been loaded.
                     script = document.createElement('script');
                     script.async = true;
@@ -84,12 +102,14 @@ var include = (function () {
                         if (this.readyState === 'loaded' && include.autoRemove) {
                             removeScriptTag(script);
                         }
+                        callProgressStatus(sourceURL, name, 'loaded');
                         asynCall(fn);
                     };
                     script.onload = function () {
                         if (include.autoRemove) {
                             removeScriptTag(script);
                         }
+                        callProgressStatus(sourceURL, name, 'loaded');
                         asynCall(fn);
                     };
                     if (include.xlink === true) {
@@ -128,6 +148,8 @@ var include = (function () {
         var index = 0,
             len = req.length,
             isGlobal = srcPath[name] && srcPath[name].global;
+
+        remaining += len;
 
         if (!len) {
             if (isGlobal) {
@@ -204,6 +226,21 @@ var include = (function () {
             srcPath[name] = { path: path };
         }
     };
+
+    /**
+     * status
+     * this function will be called twice per file.
+     * it's called once the loading of the file started and it will called once the file has been successfully loaded.
+     * @param options  will contains 3 properties.
+     * {
+     *      "filename": <name of the file>
+     *      "status": <"loading" | "loaded">,
+     *      "remaining": <number of item remaining to be loaded>
+     * }
+     */
+    implInclude.status = function (options) {};
+
+    implInclude.VERSION = '1.0.1';
 
     return implInclude;
 }());
